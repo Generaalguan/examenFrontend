84761456be75c8a909a280975c8a5769204b97c4
Generaalguan <33372@ma-web.nl>
2023-12-04 15:15:51 +0100

    homePage is responsive

--- sprint1/code/js/app.js
+++ sprint1/code/js/app.js
@@ -5,14 +5,21 @@ const menu = Vue.createApp({
             search: false
         }
     }, methods: {
+        //menu
         togglemenu() {
+            this.menu = !this.menu;
+        },
+        togglemenuSmall() {
             this.menu = !this.menu;
             this.search = this.search = false;
         },
+        //search
         togglesearch() {
             this.search = !this.search;
+        },
+        togglesearchSmall() {
+            this.search = !this.search;
             this.menu = this.menu = false;
-
         }
     }
 });
